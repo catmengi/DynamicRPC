478b26ddbc0c9d1e6dbd088e67ad8ff3c1494073
Catmengi <138163939+catmengi@users.noreply.github.com>
2024-08-13 23:53:16 +0300

    Update rpcclient.h

--- rpcclient.h
+++ rpcclient.h
@@ -7,7 +7,13 @@ struct rpccon{
    pthread_t ping;
    pthread_mutex_t send;
 };
+struct rpcclient_fninfo{
+   enum rpctypes* proto;
+   uint64_t protolen;
+   char* name;
+};
 int rpcserver_connect(char* host,char* key,int portno,struct rpccon* con);
 int rpcclient_call(struct rpccon* con,char* fn,enum rpctypes* rpctypes,char* flags, int rpctypes_len,void* fnret,...);
 void rpcclient_discon(struct rpccon* con);
-char** rpcclient_list_functions(struct rpccon* con,uint64_t* fn_len);
+struct rpcclient_fninfo* rpcclient_list_functions(struct rpccon* con,uint64_t* fn_len);
+void rpcclient_fninfo_free(struct rpcclient_fninfo* in,uint64_t len);
