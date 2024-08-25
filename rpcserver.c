35d83dd6deb6f84379c50e6bf9014b7b436dd9c3
Catmengi] <catengineerforever@gmail.com>
2024-08-26 01:23:50 +0300

    little fix of exit mark in rpcserver.c

--- rpcserver.c
+++ rpcserver.c
@@ -465,6 +465,7 @@ exit:
     tqueque_free(rpcbuff_upd);
     tqueque_free(rpcbuff_free);
     tqueque_free(_rpcstruct_free);
+    tqueque_free(strret_free);
     for(uint8_t i = 0; i < cfn->nargs; i++){
         free(callargs[i]);
     }
