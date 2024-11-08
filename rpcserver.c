bc22f06d8e12dcad231040e547b1cf1f2a7bd112
Catmengi <catengineerforever@gmail.com>
2024-11-08 15:38:22 +0300

    add uniq to massages of succeded auth

--- rpcserver.c
+++ rpcserver.c
@@ -568,7 +568,7 @@ void* rpcserver_client_thread(void* arg){
             type_to_arr(reply.payload,&uniq);
             free(uniq.data);
             if(send_rpcmsg(&reply,thrd->client_fd) != 0) goto exit;
-            printf("%s: auth ok, OK is replyied to client\n",__PRETTY_FUNCTION__);
+            printf("%s: auth ok, OK is replyied to client (%s)\n",__PRETTY_FUNCTION__,thrd->client_uniq);
             if(thrd->serv->newclient_cb != NULL)
                 thrd->serv->newclient_cb(thrd->serv->newclient_cb_data,thrd->addr);
 
