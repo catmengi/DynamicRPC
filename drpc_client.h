f57934f1c8ded40dedadaa21defd8dc5364db685
Catmengi <138163939+catmengi@users.noreply.github.com>
2025-03-30 13:21:28 +0300

    Update drpc_client.h

--- drpc_client.h
+++ drpc_client.h
@@ -29,7 +29,7 @@ void drpc_client_disconnect(struct drpc_client* client);  //disconnects and free
 
 int drpc_client_call(struct drpc_client* client, char* fn_name, enum drpc_types* prototype, size_t prototype_len,void* native_return,...);
                                                             /*
-                                                            * fn_name - name of client functions
+                                                            * fn_name - name of function to be called
                                                             * prototype - function prototype, used to parse variable arguments
                                                             * prototype_len - len of prototype
                                                             * native_return - pointer to chunk of memory where function return  will be placed
