f318abfe3ab3b448bd8c1bf9cfc6401f31652878
Catmengi <catengineerforever@gmail.com>
2025-04-06 17:43:09 +0300

    comment addition

--- drpc_server.h
+++ drpc_server.h
@@ -159,7 +159,7 @@ void drpc_server_register_proxy_fn(struct drpc_server* server,struct drpc_client
                                                                                                                   // prototype - prototype of proxy function
                                                                                                                   // prototype_len - length of prototype
 
-//NOTE: You SHOULD NOT disconnect proxy client yourself because it will cause double-free or other errors, it will be done automaticly
+//NOTE: You SHOULD NOT disconnect proxy client yourself because it will cause double-free or other errors, it will be done automaticly on drpc_server_free
 //======================================================================================================================================================================================
 
 void drpc_server_set_proxy_fail_cb(struct drpc_server* server, drpc_proxy_fail_handler fail_handler); //set proxy client fail callback. Which should return: 0 - success reconnect, NOT 0 - error
